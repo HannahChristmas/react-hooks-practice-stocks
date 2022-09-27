9bc799bba0e9207c4cf676c749870e126346b5f2
HannahChristmas <hc109909@gmail.com>
2022-09-27 13:33:57 -0400

    commit tree 6580ce4699637b3efd7730a1116995304d1e71f3

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,83 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+const [stocks, setStocks] = useState([])
+const [portfolioStocks, setPortfolioStocks] = useState([])
+const [stockFilter, setStockFilter] = useState('')
+const [sortBy, setSortBy] = useState('')
+
+
+useEffect(() => {
+  fetch("http://localhost:3001/stocks")
+  .then((res) => res.json())
+  .then((data) => (setStocks(data)))
+}, [])
+
+useEffect(() => {
+  if(sortBy === 'Alphabetically'){
+    const sortedStocks = sortByName()
+    setStocks(sortedStocks)
+  } else {
+    const sortedStocks = sortByPrice()
+    setStocks(sortedStocks)
+  }
+}, [sortBy])
+
+const sortStocks = (e) => {
+  setSortBy(e.target.value)
+}
+
+const sortByName = () => {
+  return [...stocks].sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+  });
+}
+
+const sortByPrice = () => {
+  return [...stocks].sort((a, b) => a.price - b.price);
+}
+
+const buyStock = (stock) => {
+  if(!portfolioStocks.includes(stock)){
+    const updatedPortfolioStocks = [...portfolioStocks, stock]
+    setPortfolioStocks(updatedPortfolioStocks)
+  } else {
+    alert("You've already added this. Chill.")
+  }
+}
+
+const sellStock = (stock) => {
+   const updatedPortfolioStocks = portfolioStocks.filter(myStock => myStock.id !== stock.id) 
+   setPortfolioStocks(updatedPortfolioStocks)
+}
+  
+function setFilter(e) {
+  setStockFilter(e.target.value)
+}
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar setFilter={setFilter} sortStocks={sortStocks} sortBy={sortBy}/>
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stockFilter={stockFilter} stocks={stocks} handleClick={buyStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolioStocks} handleClick={sellStock}/>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks, handleClick, stockFilter}) {
+
+const filteredStocks = stocks.filter(stock => {
+  if (stockFilter === "") return true
+  if (stockFilter === stock.type) return true
+})
+
+  const stocksOnLoading = filteredStocks.map((stock) => {
+    return(
+      <Stock handleClick={() => handleClick(stock)} key={stock.id} stock={stock}/>
+    )
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocksOnLoading}
     </div>
   );
 }
